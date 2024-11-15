53ab7dce502d3f120245d14008fd245b4b15810a
Rajat Malik <133639198+Rajat-Malik2389@users.noreply.github.com>
2024-11-15 14:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(str == ""){
+    return null;
+  }
+  let flag = true;
+  let res = [];
+  let freq = {};
+  let arr = str.split("");
+  
+  
+  for(let char of arr){
+    if(freq[char]){
+        freq[char]++;
+    }
+    else{
+        freq[char] = 1;
+    }
 }
+ 
+for(char of arr){
+    if(freq[char] == 1){
+        return char;
+    }
+    
+    res.push(freq[char]);
+  
+}
+ 
+for(let char of res){
+  if(char != 1){ //here i assume there is no char whose frequency is one
+    flag = false;
+  }
+  else{
+    flag = true;
+  }
+}
+if(flag == false){
+  return null;
+}
+}
+ 
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
+ 
+ 
+ 
+//sir ye daalna, without return statement ke likha hai
+
+
+
